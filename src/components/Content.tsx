920c19ae2c8fed34fd9fb6efc23c6dca9b9359ef
Raphael <rapha35om.rm@gmail.com>
2022-06-15 17:36:17 -0300

    first commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,43 @@
+import { MovieCard } from "../components/MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+interface ContentProps {
+  selectedGenre: {
+    title: string;
+  };
+  movies: Array<MovieProps>;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
